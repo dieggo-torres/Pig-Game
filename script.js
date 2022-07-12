429d24298264cc74c22c33bd7299a0891e490c37
unknown <dgmouratorres@gmail.com>
2022-07-12 11:21:17 -0300

    Switch players if active player gets a 1

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -15,7 +17,11 @@ score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+
 let currentScore = 0;
+
+let activePlayer = 0;
 
 // Rolling dice functinality
 btnRoll.addEventListener('click', function () {
@@ -30,7 +36,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
